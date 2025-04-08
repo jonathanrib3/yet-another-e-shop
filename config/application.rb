cffddfe35cf98d08af0d5e70b1711278e2acd1dd
jonathanrib3 <jonathanrib.3@gmail.com>
2025-04-08 11:27:10 -0300

    chore: add needed configs to admin-user-update-and-auth modules

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,10 @@ module YetAnotherEShop
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    config.eager_load_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("app/errors")
+
+    config.autoload_paths << Rails.root.join("app/controllers/v1/concerns")
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
