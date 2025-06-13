bd20de965ae734f979e278549e01992a30c3cecd
jonathanrib3 <jonathanrib.3@gmail.com>
2025-06-13 10:56:33 -0300

    Refactor code structure for improved readability and maintainability

---
+++ app/controllers/v1/customers_controller.rb
@@ -0,0 +1,74 @@
+module V1
+    class CustomersController < V1::ApplicationController
+      include Authenticator
+
+      before_action :authenticate_user!, only: [ :update, :destroy, :show ]
+
+      def create
+        ActiveRecord::Base.transaction do
+          @customer = Customer.new(
+            **create_customers_params.to_h.deep_symbolize_keys.except(:user, :addresses),
+            user_attributes: create_customers_params["user"],
+            addresses_attributes: create_customers_params["addresses"] || {}
+          )
+          @customer.save!
+          UserMailer.confirmation_email(@customer.user).deliver_later
+          Stripe::SendCustomerDataJob.perform_async(@customer.id)
+
+          render @customer, status: :created
+        end
+      end
+
+      def update
+        @customer = Customer.includes(:addresses, :user).find(params[:id])
+        authorize @customer, policy_class: V1::CustomerPolicy
+        Customers::Update.new(
+          customer: @customer,
+          update_customer_data: update_customers_params.to_h.deep_symbolize_keys
+        ).call
+
+        render @customer.reload, status: :ok
+      end
+
+      def destroy
+        @customer = Customer.find(params[:id])
+        authorize @customer, policy_class: V1::CustomerPolicy
+        @customer.destroy!
+
+        head :no_content
+      end
+
+      def show
+        @customer = Customer.includes(:addresses, :user).find(params[:id])
+        authorize @customer, policy_class: V1::CustomerPolicy
+
+        render @customer, status: :ok
+      end
+
+      private
+
+      def create_customers_params
+        params.require(:customer)
+          .permit(
+            :first_name, :last_name, :phone_number,
+            :date_of_birth, :document_number, :document_type,
+            user: [ :email, :password ],
+            addresses: [ [ :line_1, :line_2, :zip_code, :city, :state, :country, :address_type ] ]
+          )
+      end
+
+      def update_customers_params
+        params.require(:customer)
+          .permit(
+            :first_name, :last_name, :phone_number,
+            :date_of_birth, :document_number, :document_type,
+            user: [ :email, :password ],
+            addresses: {
+              create: [ :line_1, :line_2, :zip_code, :city, :state, :country, :address_type ],
+              update: [ :id, :line_1, :line_2, :zip_code, :city, :state, :country, :address_type  ],
+              delete: []
+            }
+          )
+      end
+    end
+end
